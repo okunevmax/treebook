bbc99a49f0acd53002027af8b3153155790ef626
maxim okunev <okunevmax@gmail.com>
2013-07-12 21:04:17 +0300

    Status updates for current user

--- test/functional/statuses_controller_test.rb
+++ test/functional/statuses_controller_test.rb
@@ -39,6 +39,16 @@ end
     assert_redirected_to status_path(assigns(:status))
   end
 
+  test "should create status for current user when logged in" do
+  sign_in users(:jason)
+
+    assert_difference('Status.count') do
+      post :create, status: { contet: @status.contet}
+    end
+
+    assert_redirected_to status_path(assigns(:status))
+  end
+
   test "should show status" do
     get :show, id: @status
     assert_response :success
